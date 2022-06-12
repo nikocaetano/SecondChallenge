d8eadfd666706a933a32fe5084b89399eb2bf131
niko caetano <ellercaetano@gmail.com>
2022-06-12 14:52:15 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let age = 24
+if (age >= 18){
+    console.log('Você tem acesso')
+} else{
+    console.log('Você não tem acesso')
+}
+
+let idade = 24
+let condicaoHumana = true
+if (idade >= 18 && condicaoHumana === true){
+    console.log('Você tem acesso')
+} else{
+    console.log('Você não tem acesso')
+}
+
+let mesAniversario = 01
+let outroMes = 12
+if (mesAniversario == 01 || outroMes !== 12){
+    console.log('Você faz aniversário em janeiro')
+} else{
+    console.log('Você faz aniversário em dezembro')
+}
